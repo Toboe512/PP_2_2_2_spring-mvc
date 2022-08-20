8033f839ee7e9d54465ba97985761d0b90e7edf2
Toboe512 <spown@list.ru>
2022-08-20 19:01:16 +0300

    Initial commit

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,32 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.models.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    @GetMapping("/car")
+    public String getCar(@RequestParam(value = "count", required = false) String name, ModelMap model) {
+
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("Модель1", 1, "карсный"));
+        cars.add(new Car("Модель2", 2, "чёрный"));
+        cars.add(new Car("Модель3", 3, "зелёный"));
+        cars.add(new Car("Модель4", 4, "белый"));
+        cars.add(new Car("Модель5", 5, "синий"));
+
+        model.addAttribute("cars", (name != null) ? cars
+                .stream()
+                .limit(Integer.parseInt(name))
+                .toList() : cars);
+        return "car";
+    }
+}
